fe41dc184a1be0697a031a4296d57f1f7011fc34
Onur YORULMAZ <onuryorulmaz9@gmail.com>
2025-03-04 12:57:27 +0300

    Update firebase.py

--- helpers/firebase.py
+++ helpers/firebase.py
@@ -9,16 +9,14 @@ import json
 from helpers import db
 from helpers import log
 
-config = db.read_config_json()
-clientCode = config.get("clientCode")
-
-PROJECT_ID = "nobetekranlari"
-CLIENT_ID = clientCode
-BASE_URL = ("https://firestore.googleapis.com/v1/projects/nobetekranlari/databases/("
-            "default)/documents/Clients/F4A7CED7-D2EB-419E-9F5D-002723F81645/ClientCommands/")
-
 
 def get_firestore_documents():
+    config = db.read_config_json()
+    clientCode = config.get("clientCode")
+
+    BASE_URL = ("https://firestore.googleapis.com/v1/projects/nobetekranlari/databases/("
+                "default)/documents/Clients/" + clientCode + "/ClientCommands/")
+
     response = requests.get(BASE_URL)
     if response.status_code == 200:
         return response.json().get("documents", [])
@@ -27,6 +25,13 @@ def get_firestore_documents():
 
 def listen_firestore(interval=10):
     log.writelog("firebase dinlemesi başladı.")
+
+    config = db.read_config_json()
+    clientCode = config.get("clientCode")
+
+    BASE_URL = ("https://firestore.googleapis.com/v1/projects/nobetekranlari/databases/("
+                "default)/documents/Clients/" + clientCode + "/ClientCommands/")
+
     while True:
         try:
             docs = get_firestore_documents()
@@ -41,10 +46,9 @@ def listen_firestore(interval=10):
                 if commandId == 1:
                     subprocess.Popen(["sudo sh /home/farma/enobet/reboot.sh"], stdout=subprocess.PIPE,
                                      stderr=subprocess.PIPE, shell=True).communicate()
+                # Open Teamviewer
                 elif commandId == 2:
                     os.system("nohup sh /home/farma/enobet/teamviewer.sh &")
-                elif commandId == 3:
-                    log.writelog("command 3")
                 else:
                     log.writelog("unknown command")
 
