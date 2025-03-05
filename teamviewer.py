3bdae3c730ab447d374c170b58bcd8b2495605b0
Onur YORULMAZ <onuryorulmaz9@gmail.com>
2025-03-05 10:13:43 +0300

    Update teamviewer.py

--- teamviewer.py
+++ teamviewer.py
@@ -8,6 +8,7 @@ import time
 import requests
 
 from helpers import db
+from helpers import log
 
 TEAMVIEWER_URL = "https://cdn.e-nobet.com/app/teamviewer_qs.tar.gz"
 DOWNLOAD_DIR = "/home/farma/enobet/"
@@ -19,6 +20,7 @@ SCREENSHOT_PATH = os.path.join(DOWNLOAD_DIR, "screenshot.png")
 
 
 def clean_directories():
+    log.writelog("Teamviewer klasör ve dosyalar temizleniyor.")
     if os.path.exists(ARCHIVE_PATH):
         subprocess.run(["sudo", "rm", "-f", ARCHIVE_PATH], check=True)
         subprocess.run(["sudo", "rm", "-rf", TEAMVIEWER_DIR], check=True)
@@ -26,12 +28,14 @@ def clean_directories():
 
 def download_teamviewer():
     try:
+        log.writelog("Downloading Teamviewer QS...")
         subprocess.run(["wget", TEAMVIEWER_URL], check=True)
     except Exception as e:
         print(e)
 
 
 def extract_teamviewer():
+    log.writelog("Extracting Teamviewer QS...")
     if not os.path.exists(ARCHIVE_PATH):
         raise FileNotFoundError("Dosya indirilemedi: " + ARCHIVE_PATH)
 
@@ -39,9 +43,11 @@ def extract_teamviewer():
 
 
 def run_teamviewer():
+    log.writelog("Running Teamviewer QS...")
     teamviewer_path = os.path.join(TEAMVIEWER_DIR, "teamviewer")
 
     if not os.path.exists(teamviewer_path):
+        log.writelog("Teamviewer QS not found...")
         raise FileNotFoundError("Çalıştırılabilir dosya bulunamadı: " + teamviewer_path)
 
     # subprocess.run([teamviewer_path], check=True)
@@ -50,11 +56,13 @@ def run_teamviewer():
 
 def is_teamviewer_running():
     result = subprocess.run(["pgrep", "-f", TEAMVIEWER_PROCESS], stdout=subprocess.PIPE)
+    log.writelog("Checking if Teamviewer QS is running:" + str(result.returncode))
     return result.returncode == 0
 
 
 def capture_screenshot():
     try:
+        log.writelog("Capturing screenshot...")
         config = db.read_config_json()
         clientCode = config.get("clientCode")
 
@@ -71,9 +79,9 @@ def capture_screenshot():
             os.remove(SCREENSHOT_PATH)
             params = {"id": clientCode, "screenshot": "image/png," + base64_string}
             response = requests.post("https://api.e-nobet.com/api/Client/SaveScreenshot", json=params)
-            print(response.json())
+            log.writelog(response.json())
     except Exception as e:
-        print("Failed to capture screenshot:", e)
+        log.writelog("Failed to capture screenshot:", e)
 
 
 def main():
